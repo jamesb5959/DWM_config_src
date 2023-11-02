77dae1523c37cc599c485248b0f467326526e58e
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2023-11-02 10:13:09 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Check if the script is running with superuser privileges
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script needs to be run as sudo. Please use 'sudo' to execute it."
+    exit 1
+fi
+
 # Change directory to the 'src' folder
 cd ~/src
 
