84bfb0f63e83d807b47754573aa535b0f08e0bd2
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-03-01 01:28:57 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo "This script must be run with sudo."
+    exit 1
+
 curl -O https://blackarch.org/strap.sh
-echo 5ea40d49ecd14c2e024deecf90605426db97ea0c strap.sh | sha1sum -c
 chmod +x strap.sh
-sudo ./strap.sh
-sudo pacman -Syu
+./strap.sh
+pacman -Syu
 echo "Blackarch install completed."
 
+pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+
+echo "" >> /etc/pacman.conf
+echo "[g14]" >> /etc/pacman.conf
+echo "Server = https://arch.asus-linux.org" >> /etc/pacman.conf
+
+pacman -Suy
 # Define the required packages
-required_packages=("firefox" "discord" "feh" "xorg-xinit" "xorg-xsetroot" "exploitdb" "vim" "git" "w3m" "neofetch" "lightdm")
+required_packages=("firefox" "discord" "feh" "xorg-server" "xorg-xinit" "xorg-xsetroot" "xcompmgr" "vim" "git" "neofetch" "lightdm" "lightdm-gtk-greeter" "asusctl" "supergfxctl" "rog-control-center")
 
 # Function to check and install packages
 check_and_install_packages() {
@@ -33,7 +46,11 @@ check_and_install_packages
 
 echo "Downloading packages completed."
 
+systemctl enable --now power-profiles-daemon.service
+systemctl enable --now supergfxd
+systemctl enable lightdm.service
 # Change directory to the 'src' folder
+mv src ~/src
 cd ~/src
 
 # Compile and install in the specified order
@@ -45,6 +62,8 @@ done
 
 # Move 'xsessions' to /usr/share/
 mv xsessions /usr/share/
+cd /usr/share/xsessions/
+chmod +x startdwm
 
 # Move '.xinitrc' and '.vimrc' to your home directory
 mv .xinitrc ~
