e89f98b6ab3dd44683f8856276ae84641815c90e
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-03-04 09:57:07 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,24 +4,9 @@ if [ "$(id -u)" != "0" ]; then
     echo "This script must be run with sudo."
     exit 1
 
-curl -O https://blackarch.org/strap.sh
-chmod +x strap.sh
-./strap.sh
-pacman -Syu
-echo "Blackarch install completed."
-
-pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
-
-echo "" >> /etc/pacman.conf
-echo "[g14]" >> /etc/pacman.conf
-echo "Server = https://arch.asus-linux.org" >> /etc/pacman.conf
-
-pacman -Suy
 # Define the required packages
 required_packages=("firefox" "discord" "feh" "xorg-server" "xorg-xinit" "xorg-xsetroot" "picom" "vim" "git" "neofetch" "lightdm" "lightdm-gtk-greeter" "asusctl" "supergfxctl" "rog-control-center" "nvidia")
+required_packages_void=("firefox" "feh" "xorg-server" "xinit" "xsetroot" "picom" "vim" "git" "neofetch" "lightdm" "lightdm-gtk-greeter" "nvidia")
 
 # Function to check and install packages
 check_and_install_packages_arch() {
@@ -45,7 +30,7 @@ check_and_install_packages_arch() {
 
 check_and_install_packages_void() {
     local missing_packages=()
-    for pkg in "${required_packages[@]}"; do
+    for pkg in "${required_packages_void[@]}"; do
         if ! xbps-query -Rs "$pkg" &>/dev/null; then
             missing_packages+=("$pkg")
         fi
@@ -66,10 +51,30 @@ check_and_install_packages_void() {
 # Check and install required packages
 if grep -q 'ID=arch' /etc/os-release; then
     echo "Running on Arch Linux, proceeding..."
+    curl -O https://blackarch.org/strap.sh
+    chmod +x strap.sh
+    ./strap.sh
+    pacman -Syu
+    echo "Blackarch install completed."
+    echo "Asus linux installing."
+    pacman-key --recv-keys 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    pacman-key --lsign-key 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    pacman-key --finger 8F654886F17D497FEFE3DB448B15A6B0E9A3FA35
+    echo "" >> /etc/pacman.conf
+    echo "[g14]" >> /etc/pacman.conf
+    echo "Server = https://arch.asus-linux.org" >> /etc/pacman.conf
+    pacman -Suy
+    echo "Asus linux install completed."
     check_and_install_packages_arch
+    systemctl enable --now power-profiles-daemon.service
+    systemctl enable --now supergfxd
+    systemctl enable lightdm.service
 elif grep -q 'ID=void' /etc/os-release; then
     echo "Running on Void Linux, proceeding..."
     check_and_install_packages_void
+    systemctl enable --now power-profiles-daemon.service
+    systemctl enable lightdm.service
 else
     echo "This script is intended for Arch Linux or Void Linux. Exiting..."
     exit 1
@@ -77,9 +82,6 @@ fi
 
 echo "Downloading packages completed."
 
-systemctl enable --now power-profiles-daemon.service
-systemctl enable --now supergfxd
-systemctl enable lightdm.service
 # Change directory to the 'src' folder
 mv src ~/src
 cd ~/src
@@ -92,13 +94,14 @@ for dir in dmenu st dwm slstatus; do
 done
 
 # Move 'xsessions' to /usr/share/
+cd ~/DWM_config_src/
 mv xsessions /usr/share/
 cd /usr/share/xsessions/
 chmod +x startdwm
 
 # Move '.xinitrc' and '.vimrc' to your home directory
-mv .xinitrc ~
-mv .vimrc ~
+mv .xinitrc ~/
+mv .vimrc ~/
 
 # Rename the 'vim' folder to '.vim' and move it to your home directory
 mv vim ~/.vim
