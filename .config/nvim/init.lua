72be8a071826f46a2c61fa960a183d405c30fda2
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-05-29 14:36:09 -0600

    Add files via upload

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+--LAZY setup
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   vim.fn.system({
@@ -10,46 +11,167 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
+--
 
-vim.g.mapleader = ' ' -- Make sure to set `mapleader` before lazy so your mappings are correct
-vim.g.maplocalleader = ' ' -- Same for `maplocalleader`
+-- NVIM config
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
 vim.opt.background = "dark"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.termguicolors = true
+vim.opt.expandtab = true -- Use spaces instead of tabs
+vim.opt.tabstop = 2      -- Number of spaces tabs count for
+vim.opt.softtabstop = 2  -- Number of spaces per Tab press
+vim.opt.shiftwidth = 2   -- Number of spaces for auto-indent
+vim.g.mapleader = " "
+--
 
 --plugins
 require("lazy").setup({
- {
-  "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
+  {
+    "neanias/everforest-nvim",
+    version = false,
+    lazy = false,
+    priority = 1000, -- make sure to load this before all the other start plugins
+    -- Optional; default configuration will be used if setup isn't called.
+    config = function()
+      require("everforest").setup({
+        -- Your config here
+        vim.cmd([[colorscheme everforest]]),
+      })
+    end,
   },
-  config = function()
-    require("nvim-tree").setup {}
-  end,
- },
- {
-  "neanias/everforest-nvim",
-  version = false,
-  lazy = false,
-  priority = 1000, -- make sure to load this before all the other start plugins
-  -- Optional; default configuration will be used if setup isn't called.
-  config = function()
-    require("everforest").setup({
-      -- Your config here
-      vim.cmd([[colorscheme everforest]])
-    })
-  end,
- },
- {
-  "neovim/nvim-lspconfig",
-  version = "*",
-  lazy = false,
-  config = function()
-    require'lspconfig'.pyright.setup{}
-  end,
- }
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.6",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        auto_install = true,
+        highlight = { enable = true },
+        indent = { enable = true },
+      })
+    end,
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup({
+        view = {
+          width = 30,
+        },
+      })
+      require("nvim-web-devicons").setup({
+        -- globally enable default icons (default to false)
+        -- will get overriden by `get_icons` option
+        default = true,
+      })
+    end,
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    config = function()
+      require("lualine").setup({})
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup({})
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "pyright", "clangd" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.pyright.setup({})
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown({}),
+          },
+        },
+      })
+      require("telescope").load_extension("ui-select")
+    end,
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+      local null_ls = require("null-ls")
+      null_ls.setup({
+        sources = {
+          null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.black,
+          --require("none-ls.formatting.ruff"),
+          require("none-ls.diagnostics.ruff"),
+          --null_ls.builtins.diagnostics.ruff, 
+        },
+      })
+      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+    end,
+  },
 })
+--
+
+--Plugin configs
+--Telescope config
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+--
+--nvim-tree
+vim.keymap.set("n", "<C-n>", ":NvimTreeOpen <CR>", {})
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- Open NvimTree
+    require("nvim-tree.api").tree.open()
+    -- Switch to the main window
+    vim.cmd("wincmd p")
+  end,
+})
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    local bufnr = vim.api.nvim_get_current_buf()
+    local bufname = vim.api.nvim_buf_get_name(bufnr)
+    if bufname:match("NvimTree_") then
+      if #vim.api.nvim_list_wins() == 1 then
+        vim.cmd("quit")
+      end
+    end
+  end,
+})
+--
