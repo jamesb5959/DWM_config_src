d13b9c5baecd9914eb5b0e2f82092353d0a99d4d
Bradley James David Beltran <30552360+jamesb5959@users.noreply.github.com>
2024-07-30 08:50:50 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -12,10 +12,10 @@ read -p "Is this setup for work or personal use? (work/personal): " setup_type
 # Define the required packages
 if [[ "$setup_type" == "work" ]]; then
     required_packages=("xss-lock" "mtr" "qemu" "libvirt" "virt-manager" "qemu-full" "dnsmasq" "bridge-utils" "ttf-jetbrains-mono-nerd" "whois" "ufw" "firefox" "xwallpaper" "nsxiv" "xorg-server" "xorg-xinit" "picom" "neovim" "fd" "ripgrep" "git" "neofetch" "nvidia" "mpv" "htop" "python-pywal
-" "zsh" "zsh-syntax-highlighting")
+" "zsh")
 elif [[ "$setup_type" == "personal" ]]; then
     required_packages=("xss-lock" "mtr" "qemu" "libvirt" "virt-manager" "qemu-full" "dnsmasq" "bridge-utils" "ttf-jetbrains-mono-nerd" "whois" "ufw" "firefox" "discord" "xwallpaper" "nsxiv" "xorg-server" "xorg-xinit" "picom" "neovim" "fd" "ripgrep" "git" "neofetch" "asusctl" "supergfxctl" "rog-control-center" "nvidia" "mpv" "htop" "python-pywal
-" "zsh" "zsh-syntax-highlighting")
+" "zsh")
 else
     echo "Invalid input. Please specify 'work' or 'personal'."
     exit 1
